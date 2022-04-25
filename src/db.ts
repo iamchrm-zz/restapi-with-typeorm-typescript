9edcccf17db4db492e43b5601a783bf096eff878
Christian Messina <chrmesina@icloud.com>
2022-04-25 14:46:39 -0400

    FIX

--- src/db.ts
+++ src/db.ts
@@ -6,11 +6,11 @@ import { Product } from "./entities/Product";
 export const AppDataSource = new DataSource({
   type: "mysql",
   host: "mdb-test.c6vunyturrl6.us-west-1.rds.amazonaws.com",
-  username: process.env.USERNAME as any,
-  password: process.env.PASSWORD as any,
+  username: "bsale_test",
+  password: "bsale_test",
   port: process.env.DATABASE_PORT as any,
-  database: process.env.DATABASE as string,
-  entities: process.env.ENTITIES as any,
+  database: "bsale_test",
+  entities: [Product, Category],
   logging: true,
   synchronize: false,
 });
